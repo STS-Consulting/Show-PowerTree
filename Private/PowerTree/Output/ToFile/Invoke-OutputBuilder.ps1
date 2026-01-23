e54f2b5bebafae53b9c60dabeedbff726877a310
STS-Consulting <STS.Consulting@hotmail.com>
2026-01-22 23:02:36 -0800

    Refactor parameter formatting and improve code readability in Invoke-OutputBuilder and Show-PowerTree functions

--- Private/PowerTree/Output/ToFile/Invoke-OutputBuilder.ps1
+++ Private/PowerTree/Output/ToFile/Invoke-OutputBuilder.ps1
@@ -1,9 +1,10 @@
 function Invoke-OutputBuilder {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [TreeConfig]$TreeConfig,
-        [bool]$ShowExecutionStats
+        [bool]$ShowExecutionStats,
+        [bool]$ShowConfigurations = $true
     )
 
     # Only create OutputBuilder if we need to save to a file
@@ -12,24 +13,26 @@ function Invoke-OutputBuilder {
     }
 
     $outputBuilder = New-Object System.Text.StringBuilder
-    
+
     # Add file header
-    [void]$outputBuilder.AppendLine("# PowerTree Output")
+    [void]$outputBuilder.AppendLine('# PowerTree Output')
     [void]$outputBuilder.AppendLine("# Generated: $(Get-Date)")
     [void]$outputBuilder.AppendLine("# Path: $($TreeConfig.Path)")
     if (-not [string]::IsNullOrEmpty($TreeConfig.OutFile)) {
         [void]$outputBuilder.AppendLine("# Output File: $($TreeConfig.OutFile)")
     }
-    
-    # Get configuration lines for file output
-    $configLines = Get-TreeConfigurationData -TreeConfig $TreeConfig
-    
-    foreach ($line in $configLines) {
-        [void]$outputBuilder.AppendLine($line)
+
+    if ($ShowConfigurations) {
+        # Get configuration lines for file output
+        $configLines = Get-TreeConfigurationData -TreeConfig $TreeConfig
+
+        foreach ($line in $configLines) {
+            [void]$outputBuilder.AppendLine($line)
+        }
     }
-    
+
     if ($ShowExecutionStats) {
-        [void]$outputBuilder.AppendLine("Append the stats here later!!")
+        [void]$outputBuilder.AppendLine('Append the stats here later!!')
     }
 
     return $outputBuilder
